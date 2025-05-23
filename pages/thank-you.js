5ad882f60772d617961d0d9245e1474e75132d31
jhaprabhat50 <jhaprabhat50@gmail.com>
2025-05-23 11:52:36 +0530

    Update thank-you.js

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,1 +1,11 @@
+const ThankYou = () => (
+  <div className="min-h-screen flex flex-col items-center justify-center text-center p-6 bg-green-50">
+    <h1 className="text-4xl font-bold text-green-700 mb-4">Thank You!</h1>
+    <p className="text-lg text-gray-700 mb-6">
+      Your message has been received. We'll get back to you shortly.
+    </p>
+    <a href="/" className="text-blue-600 underline">Back to Home</a>
+  </div>
+);
+export default ThankYou;
 
